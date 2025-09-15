a78e1b680a6ab3cc6bec52f45062a1facad68103
Chase Maciel <macielc26@student.jhs.net>
2025-09-15 11:48:46 -0700

    auuuugh

---
+++ domestic_pigeon/Bird.lua
@@ -0,0 +1,46 @@
+Bird = Class{}
+
+local GRAVITY = 6
+
+function Bird:init()
+    self.image = love.graphics.newImage('graphics/bird.png')
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+
+    self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
+    self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+
+    self.dy = 0
+end
+
+function Bird:collides(pipe)
+    -- the 2's are left and top offsets
+    -- the 4's are right and bottom offsets
+    -- both offsets are used to shrink the bounding box to give the player
+    -- a little bit of leeway with the collision
+    if (self.x + 2) + (self.width - 4) >= pipe.x and self.x + 2 <= pipe.x + PIPE_WIDTH then
+        if (self.y + 2) + (self.height - 4) >= pipe.y and self.y + 2 <= pipe.y + PIPE_HEIGHT then
+            return true
+        end
+    end
+
+    return false
+end
+
+function Bird:update(dt)
+    self.dy = self.dy + GRAVITY * dt 
+
+    -- TODO: add mouse functionality
+    if love.keyboard.wasPressed('space') then 
+        self.dy = -1
+        sounds['jump']:play()
+    end
+
+    self.y = self.y + self.dy
+end
+
+function Bird:render()
+    love.graphics.draw(self.image, self.x, self.y)
+
+    -- TODO: add debug option for printing hitbox
+end
---
+++ domestic_pigeon/PipePair.lua
@@ -0,0 +1,33 @@
+PipePair = Class{}
+
+local GAP_HEIGHT = 90
+
+function PipePair:init(y)
+    self.x = VIRTUAL_WIDTH + 32
+    self.y = y
+
+    -- TODO: finish implementing constructor
+    self.pipes = {
+        ['upper'] = Pipe('top', y),
+        ['lower'] = Pipe('bottom', y + PIPE_HEIGHT + GAP_HEIGHT)
+    }
+
+    self.remove = false
+    self.scored = false
+end
+
+function PipePair:update(dt)
+    if self.x > -PIPE_WIDTH then
+        self.x = self.x - PIPE_SPEED * dt
+        self.pipes['lower'].x = self.x
+        self.pipes['upper'].x = self.x
+    else
+        self.remove = true
+    end
+end
+
+function PipePair:render()
+    for k, pipe in pairs(self.pipes) do
+        pipe:render()
+    end
+end
